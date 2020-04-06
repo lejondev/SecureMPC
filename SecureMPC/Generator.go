de3d6c1baa8e1ef4d53cd59402e622e341e0e887
lejondev <mail@lejon.dev>
2020-04-06 11:49:08 +0200

    added some comments, not done yet

--- SecureMPC/Generator.go
+++ SecureMPC/Generator.go
@@ -8,8 +8,11 @@ import (
 var One = big.NewInt(1)
 var Two = big.NewInt(2)
 
-var PublicExponent = big.NewInt(65537)
+// Public exponent is a prime e > l
+var e = big.NewInt(65537)
 
+// GeneratePrimes will generate two random primes n=p*q and m=p'*q'
+// security is the length of the primes p,q used
 func GeneratePrimes(security int) (*big.Int, *big.Int) {
 
 	p1, _ := rand.Prime(rand.Reader, security)
@@ -28,13 +31,18 @@ func GenerateRandomQuadratic(n *big.Int) *big.Int {
 	return new(big.Int).Mul(v, v)
 }
 
+// Generates the parts of the RSA key, where PubKey=(n,e) and SecKey=(n,d)
+// n is the RSA modulus
+// e is the public exponent
+// d is the private exponent
+// m is the RSA message to be signed / encrypted
 func GenerateRSAKey(security int) (*big.Int, *big.Int, *big.Int, *big.Int) {
 	n, m := GeneratePrimes(security)
 
 	// This mod inverse should not be able to fail, as m should be a product of two primes, none of which can be equal to e
-	secretKey := new(big.Int).ModInverse(PublicExponent, m)
+	d := new(big.Int).ModInverse(e, m)
 
-	return n, PublicExponent, secretKey, m
+	return n, e, d, m
 }
 
 type BigPolynomial struct {
@@ -53,6 +61,8 @@ func (p *BigPolynomial) eval(x *big.Int, base *big.Int) *big.Int {
 	return new(big.Int).Mod(constant, base)
 }
 
+// GenerateRandomBigPolynomial creates a random polynomial, such that f(0)=d, where
+// d is the secret to be shared (shamir secret sharing)
 func GenerateRandomBigPolynomial(zero *big.Int, base *big.Int, degree int) *BigPolynomial {
 	coefs := make([]*big.Int, degree)
 	for i := 0; i < degree; i++ {
@@ -64,6 +74,7 @@ func GenerateRandomBigPolynomial(zero *big.Int, base *big.Int, degree int) *BigP
 	}
 }
 
+// GenerateSecretShares will use a polynomial to generate secret key shares
 func GenerateSecretShares(poly *BigPolynomial, base *big.Int, l int) []*big.Int {
 	shares := make([]*big.Int, l+1)
 	for i := 1; i <= l; i++ {
